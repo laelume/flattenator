d9d6ee1f0652849c61f3245282b188644044c9ed
ashlae blume <45116836+laelume@users.noreply.github.com>
2020-04-15 10:59:37 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append("/Users/timehopper/Documents/foxdot/flattenator/helpers")
+#import depthcharge, findcurly, inbetween, nextval, translator
+global pattern
+pattern = 'x -{ }{-x {w-x} }w-{x{xo}  }-x --'
